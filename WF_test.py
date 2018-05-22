cab07624074277008eb2f5ef380b7c47756ee14c
DemonAuditore <wxxhelloworld@hotmail.com>
2018-05-22 16:15:19 +0800

    20180522

--- WF_test.py
+++ WF_test.py
@@ -37,25 +37,66 @@ def foo():
         file_i.append(df)
     return file_i
 
-def find_key_words_from_text(content, rank):
-    r = Rake(ranking_metric=Metric.TF_IDF,rank = rank)
+def find_key_words_from_text(content, Compare):
+    r = Rake(ranking_metric=Metric.TF_IDF, COMP_FLAG=Compare)
     r.extract_keywords_from_list(content)
     return r.get_ranked_phrases_with_scores()
 
-def entity_reco():
-    pass
+def self_excel_writer(COMP, fileName, content):
+    s = 'keywords_{a}.xlsx'
+    writer = pd.ExcelWriter(s.format(a=fileName.split('.')[0]))
+    if COMP:
+        entities_list, words_list, whole_list = find_key_words_from_text(content, COMP)
+        ##  unzip data
+        entities_list = list(zip(*entities_list))
+        words_list = list(zip(*words_list))
+        whole_list = list(zip(*whole_list))
+        ##  write data into excel files
+        if whole_list:
+            # whole keywords list
+            whole_dict = {'热词': list(whole_list[0]),
+                          'TF-IDF': list(whole_list[1]),
+                          '词频': list(whole_list[2])
+                          }
+            data_whole = DataFrame(whole_dict)
+            data_whole.to_excel(writer, 'keywords', index=False)
+        if entities_list:
+            # entities keywords list
+            entities_dict = {'实体热词': list(entities_list[0]),
+                             'TF-IDF-实体': list(entities_list[1]),
+                             '词频-实体': list(entities_list[2])
+                             }
+            data_entities = DataFrame(entities_dict)
+            data_entities.to_excel(writer, 'entities', index=False)
+        if words_list:
+            # non-entities keywords list
+            words_dict = {'非实体热词': list(words_list[0]),
+                          'TF-IDF-非实体': list(words_list[1]),
+                          '词频-非实体': list(words_list[2])
+                          }
+            data_words = DataFrame(words_dict)
+            data_words.to_excel(writer, 'non-entities', index=False)
+    else:
+        if key_words_list:
+            key_words_list = find_key_words_from_text(content, COMP)
+            key_words_list = list(zip(*key_words_list))
+            key_words_dict = {'热词': list(key_words_list[0]),
+                              'TF-IDF': list(key_words_list[1]),
+                              '词频': list(key_words_list[2])
+                              }
+            data_keywords = DataFrame(key_words_dict)
+            data_keywords.to_excel(writer, 'keywords', index=False)
+    writer.save()
+
 
 if __name__ == "__main__":
+    COMP = True
     file_i = foo()
     for i,file in enumerate(file_i):
-        head_data = file.head(10)
+        head_data = file.head(200)
         content = DataFrame(head_data, columns=[col_name])
-        key_words_list = find_key_words_from_text(content, 20)
-        key_words_dict = {'热词': [key_word[1] for key_word in key_words_list],'WORD-FREQUENCY':[key_word[0] for key_word in key_words_list]}
-        data = DataFrame(key_words_dict)
-        s = 'keywords_{a}.csv'
-        data.to_csv(s.format(a=file_name[i].split('.')[0]), index=False, sep=',')
-
+        fileName = file_name[i]
+        self_excel_writer(COMP, fileName, content)
 
 
 
--- rake_test.py
+++ rake_test.py
@@ -11,6 +11,9 @@ from itertools import chain, groupby, product
 import nltk
 from enum import Enum
 from nltk.tokenize import wordpunct_tokenize
+from nltk import ne_chunk, ne_chunk_sents, pos_tag, word_tokenize
+from nltk.chunk.regexp import RegexpChunkRule
+from nltk.chunk.api import ChunkParserI
 
 from pandas import Series, DataFrame
 from collections import defaultdict
@@ -36,7 +39,6 @@ JAVA_PATH = "C:/Program Files/Java/jdk1.8.0_92/bin/java.exe"
 os.environ['JAVAHOME'] = JAVA_PATH
 
 
-
 class Metric(Enum):
     """Different metrics that can be used for ranking."""
 
@@ -55,7 +57,7 @@ class Rake(object):
             language="english",
             ranking_metric=Metric.WORD_FREQUENCY,
             col_name='内容',
-            rank = 1,
+            COMP_FLAG = False,
 
     ):
         """Constructor.
@@ -80,43 +82,50 @@ class Rake(object):
             self.punctuations = string.punctuation
 
         # All things which act as sentence breaks during keyword extraction.
-        self.to_ignore = set(chain(self.stopwords, self.punctuations,[',"']))
+        self.to_ignore = set(chain(self.stopwords, self.punctuations))
 
         # Stuff to be extracted from the provided text.
         self.frequency_dist = None
         self.tfidf_dict = None
         # self.frequency_dict = None
-        self.NE_dict = defaultdict(lambda : 'O')
+        # self.NE_dict = defaultdict(lambda : 'O')
         self.tf_dict = None
         self.degree = None
         self.rank_list = None
         self.ranked_phrases = None
+        self.word_list = None
+        self.entities_set = None
 
         # Stuff for pandas
         self.col_name = col_name
-        self.rank = rank
+        self.COMP_FLAG = COMP_FLAG
 
     def extract_keywords_from_list(self, content):
         """Method to extract keywords from the text list.
         :param text: Text to extract keywords from, provided as a string.
         """
         text_list = self.get_textlist_from_dataframe(content)
+        entities_set = set()
         phrase_list = []
         for text in text_list:
-            if not self.language_identification(text):
-                continue
-            phrase_list_text = self.extract_keywords_from_text(text)
+            # if not self.language_identification(text):
+            #     continue
+            phrase_list_text, entities_text = self.extract_keywords_from_text(text)
             phrase_list.append('`'.join(list(phrase_list_text)))
-
+            entities_set.update(entities_text)
+        self.entities_set = entities_set
+        # if phrase_list:
+        #     if self.metric == Metric.TF_IDF or self.metric == Metric.TERM_FREQUENCY:
+        #         self._build_tfidf_list(phrase_list)
+        #
+        #     phrase_list = [val for sublist in phrase_list for val in sublist.split('`')]
+        #
+        #     if self.metric == Metric.WORD_FREQUENCY:
+        #         self._build_frequency_dist_list(phrase_list)
+        #     self._build_ranklist(phrase_list)
         if phrase_list:
-            if self.metric == Metric.TF_IDF or self.metric == Metric.TERM_FREQUENCY:
-                self._build_tfidf_dic(phrase_list)
-
-            phrase_list = [val for sublist in phrase_list for val in sublist.split('`')]
-
-            if self.metric == Metric.WORD_FREQUENCY:
-                self._build_frequency_dist_list(phrase_list)
-            self._build_ranklist(phrase_list)
+            self._build_tfidf_list(phrase_list)
+            self._build_ranklist()
 
     def language_identification(self, text):
         identifier = LanguageIdentifier.from_modelstring(model, norm_probs=True)
@@ -140,9 +149,57 @@ class Rake(object):
         """Method to extract keywords from the text provided.
         :param text: Text to extract keywords from, provided as a string.
         """
-        sentences = nltk.tokenize.sent_tokenize(text)
-        phrase_list = self.extract_keywords_from_sentences(sentences)
-        return phrase_list
+        phrase_list = []
+        entities = set()
+        toks = wordpunct_tokenize(text)
+        pos = pos_tag(toks)
+        c_toks = list(ne_chunk(pos))
+        # groups = groupby(c_toks, lambda x: isinstance(x, tuple))
+        # grammar = "NP: {<DT>?<JJ>*<NN>|<NNP>*}"
+        # cp = nltk.RegexpParser(grammar)
+        # result = cp.parse(sentences)
+
+        groups = groupby(c_toks, lambda x: isinstance(x, tuple))
+        ignore_pos = ['VBD', 'IN']
+        in_pos = ['JJ','NNP', 'NN','VB','RB']
+        # groups = [g[0] for g in groups]
+        # sub_groups = groupby(groups[0][1], lambda x: x[1]=='VBD'or x[1]=='IN')
+        for key, group in groups:
+            if key:
+                sub_toks = list(group)
+                sub_groups = groupby(sub_toks, lambda x: x[1] not in ignore_pos
+                                     and re.match(r'[a-zA-Z]+',x[0]) != None
+                                     and x[1] in in_pos)
+                sub_group_list = [" ".join(sg[0] for sg in sub_group[1]) for sub_group in sub_groups if sub_group[0]]
+                phrase_list.extend(sub_group_list)
+            else:
+                # gl = list(group)
+                # print(gl)
+                # sys.exit(0)
+                phrase = " ".join(t[0]  for w in group for t in w)
+                entities.add(phrase.lower())
+                phrase_list.append(phrase)
+        # print(phrase_list)
+        # sys.exit(0)
+
+        # for w in chunks:
+        #     if isinstance(w, tuple):
+        #         if w[0] not in self.to_ignore and re.match(r'[a-zA-Z0-9]+',w[0]) != None:
+        #             phrase_list.append(w[0])
+        #     else:
+        #         phrase = " ".join(t[0] for t in w)
+        #         entities.add(phrase.lower())
+        #         if 'r' in entities:
+        #             print(chunks)
+        #             print(list(chunks))
+        #             sys.exit(0)
+        #         phrase_list.append(phrase)
+
+        # phrase_list = [w[0] if isinstance(w, tuple) else " ".join(t[0] for t in w) for w in chunks]
+
+        # sentences = nltk.tokenize.sent_tokenize(text)
+        # phrase_list = self.extract_keywords_from_sentences(sentences)
+        return phrase_list, entities
 
     def extract_keywords_from_sentences(self, sentences):
         """Method to extract keywords from the list of sentences provided.
@@ -159,14 +216,16 @@ class Rake(object):
         """
         return self.ranked_phrases
 
-    def get_ranked_phrases_with_scores(self, NE_FLAG = False):
+    def get_ranked_phrases_with_scores(self):
         """Method to fetch ranked keyword strings along with their scores.
         :return: List of tuples where each tuple is formed of an extracted
                  keyword string and its score. Ex: (5.68, 'Four Scoures')
         """
-        if not self.rank_list or len(self.rank_list) < self.rank:
+        if not self.whole_rank_list:
             return []
-        return self.rank_list[:self.rank]
+        if self.COMP_FLAG:
+            return self.entities_rank_list, self.words_rank_list, self.whole_rank_list
+        return self.whole_rank_list
 
     def get_word_frequency_distribution(self):
         """Method to fetch the word frequency distribution in the given text.
@@ -180,33 +239,52 @@ class Rake(object):
         """
         return self.tfidf_dict
 
-    def get_named_entities_list(self):
-        """Method to fetch the word frequency distribution in the given text.
-        :return: Dictionary (defaultdict) of the format `word -> frequency`.
-        """
+    # def get_named_entities_list(self):
+    #     """Method to fetch the word frequency distribution in the given text.
+    #     :return: Dictionary (defaultdict) of the format `word -> frequency`.
+    #     """
+    #
+    #     return [self.NE_dict[ph[1]] for ph in self.rank_list]
 
-        return [self.NE_dict[ph[1]] for ph in self.rank_list]
-
-    def _build_tfidf_dic(self, phrase_list):
+    def _build_tfidf_list(self, phrase_list):
         """Builds tfidf dictionary of the phrases in the given text list.
            :param phrase_list: List of strings where each string is a group
            of phrases that are separated by '`', for example: 'red apple`green
             apple`yellow apple'.
            :result: Dictionary
         """
-        tfidf_dict = defaultdict(lambda : 0)
-        tf_dict = defaultdict(lambda: 0)
 
         # vectorizer = CountVectorizer(token_pattern='(?u)\w+[\s\`]\w+')
         self_tokenizer = lambda doc: re.split('`',doc)
-        vectorizer = CountVectorizer(tokenizer=self_tokenizer)
+        vectorizer = CountVectorizer(stop_words=self.to_ignore, tokenizer=self_tokenizer)
         transformer = TfidfTransformer(smooth_idf=True)
-        tfidf = transformer.fit_transform(vectorizer.fit_transform(phrase_list))
+        tf = vectorizer.fit_transform(phrase_list)
+        tfidf = transformer.fit_transform(tf)
+
+        # # calculate the coefficient matrix
+        # b = np.nonzero(tfidf.toarray())
+        # mylist = list(b[1])
+        # c = [mylist.count(item) for item in set(mylist)]
+
 
         words = vectorizer.get_feature_names()
-        weight = list(tfidf.toarray())
 
-        self.tfidf_dict = tfidf_dict
+        # tfidf_value = tfidf.toarray().sum(axis=0)
+        tfidf_value = list(tfidf.toarray().sum(axis=0))
+        tf_value = list(tf.toarray().sum(axis=0))
+
+        # print(words)
+        # print(tfidf_value)
+        # print(tf_value)
+        # tfidf_list = zip(words, tfidf_value, tf_value)
+        # tfidf_list = sorted(tfidf_list, key=lambda item: item[1])
+        # print(tfidf_list[:30])
+        #
+        # sys.exit(0)
+
+        self.word_list = words
+        self.tfidf_list = tfidf_value
+        self.tf_list = tf_value
 
 
     def _build_frequency_dist_list(self, phrase_list):
@@ -222,7 +300,7 @@ class Rake(object):
 
 
 
-    def _build_ranklist(self, phrase_list):
+    def _build_ranklist(self):
         """Method to rank each contender phrase using the formula
         For TF_IDF:
               phrase_score = tf-idf value of the phrase.
@@ -232,22 +310,46 @@ class Rake(object):
         :param phrase_list: List of List of strings where each sublist is a
                             collection of words which form a contender phrase.
         """
-        self.rank_list = []
+
+        words = self.word_list
+        tfidf_value = self.tfidf_list
+        tf_value = self.tf_list
+
+        words_rank_list = []
+        entities_rank_list = []
+
+
+        tfidf_list = zip(words, tfidf_value, tf_value)
+        tfidf_list = sorted(tfidf_list, key=lambda item: item[1], reverse=True)
+
+        for item in tfidf_list:
+            if item[0] in self.entities_set:
+                entities_rank_list.append(item)
+            else:
+                words_rank_list.append(item)
+
+        # entities_rank_list = sorted(entities_rank_list, key=lambda item: item[1])
+        # words_rank_list = sorted(words_rank_list, key=lambda item: item[1])
+
+        self.entities_rank_list = entities_rank_list
+        self.words_rank_list = words_rank_list
+        self.whole_rank_list = tfidf_list
+
         # phrase_list = [val for sublist in phrase_list for val in sublist.split('`')]
         # phrase_set = set(phrase_list)
-        phrase_list = list(set(phrase_list))
-        for phrase in phrase_list:
-            rank = 0.0
-            if self.metric == Metric.TF_IDF:
-                rank += 1.0 * self.tfidf_dict[phrase]
-            if self.metric == Metric.TERM_FREQUENCY:
-                rank += 1.0 * self.tf_dict[phrase]
-            if self.metric == Metric.WORD_FREQUENCY:
-                for word in phrase.split():
-                    rank += 1.0 * self.frequency_dist[word]
-            self.rank_list.append((rank, phrase))
-        self.rank_list.sort(reverse=True)
-        self.ranked_phrases = [ph[1] for ph in self.rank_list]
+
+        # for phrase in phrase_list:
+        #     rank = 0.0
+        #     if self.metric == Metric.TF_IDF:
+        #         rank += 1.0 * self.tfidf_dict[phrase]
+        #     if self.metric == Metric.TERM_FREQUENCY:
+        #         rank += 1.0 * self.tf_dict[phrase]
+        #     if self.metric == Metric.WORD_FREQUENCY:
+        #         for word in phrase.split():
+        #             rank += 1.0 * self.frequency_dist[word]
+        #     self.rank_list.append((rank, phrase))
+        # self.rank_list.sort(reverse=True)
+        # self.ranked_phrases = [ph[1] for ph in self.rank_list]
 
 
     def _generate_phrases(self, sentences):
@@ -282,29 +384,29 @@ class Rake(object):
         :return: List of contender phrases that are formed after dropping
                  stopwords and punctuations.
         """
-        tagger = StanfordNERTagger(
-            'E:\Learning Resources\TOOLS\stanford-ner-2015-12-09\classifiers\english.muc.7class.distsim.crf.ser.gz',
-            'E:\Learning Resources\TOOLS\stanford-ner-2015-12-09\stanford-ner.jar',
-            encoding='utf-8')
+        # tagger = StanfordNERTagger(
+        #     'E:\Learning Resources\TOOLS\stanford-ner-2015-12-09\classifiers\english.all.3class.distsim.crf.ser.gz',
+        #     'E:\Learning Resources\TOOLS\stanford-ner-2015-12-09\stanford-ner.jar',
+        #     encoding='utf-8')
+        #
+        # phrase_list = []
+        #
+        # classified_paragraphs_list = tagger.tag_sents([word_list])
+        # groups = groupby(classified_paragraphs_list[0], lambda x: x[1])
+        # for key, group in groups:
+        #     group_list = [g[0] for g in group]
+        #     if key is 'O':
+        #         # rs_groups = groupby(group_list, lambda x: x not in self.to_ignore and re.match(r'[a-zA-Z0-9]+',x) != None)
+        #         # rs_group_list = [' '.join(list(rs_group[1])) for rs_group in rs_groups if rs_group[0]]
+        #         #
+        #         # phrase_list.extend(rs_group_list)
+        #         phrase_list.extend(group_list)
+        #     else:
+        #         phrase = ' '.join(group_list)
+        #         phrase_list.append(phrase)
+        #         self.NE_dict[phrase] = key
 
-        phrase_list = []
-
-        classified_paragraphs_list = tagger.tag_sents([word_list])
-        groups = groupby(classified_paragraphs_list[0], lambda x: x[1])
-        for key, group in groups:
-            group_list = [g[0].lower() for g in group]
-            if key is 'O':
-                # rs_groups = groupby(group_list, lambda x: x not in self.to_ignore and re.match(r'[a-zA-Z0-9]+',x) != None)
-                # rs_group_list = [' '.join(list(rs_group[1])) for rs_group in rs_groups if rs_group[0]]
-                #
-                # phrase_list.extend(rs_group_list)
-                phrase_list.extend(group_list)
-            else:
-                phrase = ' '.join(group_list)
-                phrase_list.append(phrase)
-                self.NE_dict[phrase] = key
-
-        return phrase_list
+        # return phrase_list
         # return phrase_list
 
         # groups = groupby(word_list, lambda x: x not in self.to_ignore)
